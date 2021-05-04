835498668dafa8bc8a1c374a46ace4a0343f8bce
sam-may <sammay@ucla.edu>
2021-05-04 15:48:35 -0700

    Updates to data fetcher

--- autodqm_ml/data_prep/data_fetcher.py
+++ autodqm_ml/data_prep/data_fetcher.py
@@ -7,7 +7,7 @@ import pandas
 from autodqm_ml.utils import setup_logger
 
 EOS_PATH = "/eos/cms/store/group/comm_dqm/DQMGUI_data/"
-HIST_PATH = "DQMData/Run {}/CSC/Run summary/"
+HIST_PATH = "DQMData/Run {}/"
 
 class DataFetcher():
     """
@@ -51,17 +51,14 @@ class DataFetcher():
         self.logger.info("[DataFetcher : run] Running DataFetcher to grab the following set of subsystems and histograms")
         for subsystem, info in self.contents.items():
             self.logger.info("\t Subsystem: %s" % subsystem)
-            self.logger.info("\t 1d histograms:")
-            for hist in info["1d_hists"]:
-                self.logger.info("\t\t %s" % hist)
-            self.logger.info("\t 2d histograms:")
-            for hist in info["2d_hists"]:
+            self.logger.info("\t Histograms:")
+            for hist in info:
                 self.logger.info("\t\t %s" % hist)
         self.logger.info("\t for the following years %s" % str(self.years))
         self.logger.info("\t and for the following primary datasets %s" % str(self.datasets)) 
 
         self.get_list_of_files()
-        self.extract_data(short = True)
+        self.extract_data()
         self.write_data()
         self.write_summary()
 
@@ -75,7 +72,7 @@ class DataFetcher():
             self.files[pd] = {}
             for year in self.years:
                 path = self.construct_eos_path(EOS_PATH, pd, year)
-                files = self.get_files(path, short = True)
+                files = self.get_files(path, self.short)
 
                 self.logger.info("[DataFetcher : get_files] Grabbed %d files under path %s" % (len(files), path))
                 self.logger.debug("[DataFetcher : get_files] Full list of files:")
@@ -131,42 +128,40 @@ class DataFetcher():
         return files
 
 
-    def extract_data(self, short = False):
+    def extract_data(self):
         """
         Extract all requested histograms from list of files.
-        :param short: flag to grab only a couple files
-        :type short: bool
         """
         self.data = {}
         for pd in self.datasets:
             self.data[pd] = pandas.DataFrame()
             for year in self.years:
-                for subsystem, info in self.contents.items():
-                    for file in self.files[pd][year]:
-                        run_number = DataFetcher.get_run_number(file)
-                        self.logger.debug("[DataFetcher : load_data] Loading histograms from file %s, run %d" % (file, run_number))
+                for file in self.files[pd][year]:
+                    run_number = DataFetcher.get_run_number(file)
+                    self.logger.debug("[DataFetcher : load_data] Loading histograms from file %s, run %d" % (file, run_number))
 
-                        histograms_1d = self.load_data(file, run_number, subsystem, info["1d_hists"])
-                        histograms_2d = self.load_data(file, run_number, subsystem, info["2d_hists"])
+                    histograms = self.load_data(file, run_number, self.contents) 
 
-                        if histograms_1d is not None and histograms_2d is not None:
-                            columns = ["subsystem", "run_number", "pd", "year"] + histograms_1d["columns"] + histograms_2d["columns"]
-                            column_data = [[subsystem, run_number, pd, year] + histograms_1d["data"] + histograms_2d["data"]]
-                            df = pandas.DataFrame(column_data, columns = columns)
-                            self.data[pd] = self.data[pd].append(df, ignore_index=True)
-       
-                        if short:
-                            if len(self.data[pd]) > 2:
-                                return
+                    if histograms is not None:
+                        columns = ["run_number", "pd", "year"] + histograms["columns"]
+                        column_data = [[run_number, pd, year] + histograms["data"]]
+                        df = pandas.DataFrame(column_data, columns = columns)
+                        self.data[pd] = self.data[pd].append(df, ignore_index=True)
+
+                    if self.short:
+                        if len(self.data[pd]) > 2:
+                            continue
         
 
-    def load_data(self, file, run_number, subsystem, histograms):
+    def load_data(self, file, run_number, contents): 
         """
         Load specified histograms from a given file.
         :param file: dqm file
         :type file: str
         :param run_number: run number for this file
         :type run_number: int
+        :param contents: dictionary of subsystems : list of histograms to load data for
+        :type contents: dict
         :param subsystem: name of subsystem
         :type subsystem: str
         :param histograms: list of histograms to load data for
@@ -175,15 +170,28 @@ class DataFetcher():
         :rtype: dict 
         """
         hist_data = { "columns" : [], "data" : [] }
+
+        # Check if file is corrupt
+        try:
+            uproot.open(file)
+        except:
+            self.logger.info("[DataFetcher : load_data] ERROR loading file %s" % file)
+            return None
+
         with uproot.open(file) as f:
             if f is None:
                 self.logger.info("[DataFetcher : load_data] ERROR loading file %s" % file)
                 return None
 
-            for hist in histograms:
-                histogram_path = DataFetcher.construct_histogram_path(HIST_PATH, run_number, subsystem, hist) 
-                hist_data["data"].append(f[histogram_path].values())
-                hist_data["columns"].append(hist)
+            for subsystem, histogram_list in contents.items(): 
+                for hist in histogram_list:
+                    histogram_path = DataFetcher.construct_histogram_path(HIST_PATH, run_number, subsystem, hist) 
+                    hist_data["data"].append(f[histogram_path].values())
+                    hist_data["columns"].append(subsystem + "/" + hist)
+
+        self.logger.debug("[DataFetcher : load_data] Histogram contents:")
+        for hist, data in zip(hist_data["columns"], hist_data["data"]):
+            self.logger.debug("\t %s : %s" % (hist, data))
 
         return hist_data
 
@@ -219,12 +227,12 @@ class DataFetcher():
 
     def write_data(self):
         """
-        Write dataframe -> parquet file for each primary dataset.
+        Write dataframe -> pickle file for each primary dataset.
         """
         for pd in self.datasets:
             df = self.data[pd]
             if df is not None:
-                df.to_parquet("output/%s_%s.parquet" % (self.tag, pd))
+                df.to_pickle("output/%s_%s.pkl" % (self.tag, pd))
 
 
     def write_summary(self):
