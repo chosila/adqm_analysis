2f806e771e4a27d47f722f2dfec2937927f26236
sam-may <sammay@ucla.edu>
2021-10-25 10:20:37 -0700

    Make autoencoder more configurable

--- autodqm_ml/utils.py
+++ autodqm_ml/utils.py
@@ -4,6 +4,7 @@ Original author: Massimiliano Galli
 """
 
 import os
+import copy
 
 import logging
 from rich.logging import RichHandler
@@ -70,4 +71,31 @@ def expand_path(relative_path):
             break
 
     return base_path + relative_path
+
+
+def update_dict(original, new):
+    """
+    Update nested dictionary (dictionary possibly containing dictionaries)
+    If a field is present in new and original, take the value from new.
+    If a field is present in new but not original, insert this field 
+
+
+    :param original: source dictionary
+    :type original: dict
+    :param new: dictionary to take new values from
+    :type new: dict
+    :return: updated dictionary
+    :rtype: dict
+    """
+
+    updated = copy.deepcopy(original)
+
+    for key, value in original.items():
+        if key in new.keys():
+            if isinstance(value, dict):
+                updated[key] = update_dict(value, new[key])
+            else:
+                updated[key] = new[key]
+
+    return updated
 
