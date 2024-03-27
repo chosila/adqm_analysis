6cdd070a9c558cd3c596114d76bdb72b523c14e4
Chosila Sutantawibul <si_sutantawibul1@baylor.edu>
2024-03-27 21:39:35 +0100

    Revert "fixed the N=1,3,5 plots"
    This reverts commit 0c9902d54661ad1d50d0d1821be249ecdad67431.
    
    yeah I freaked out for no reason and did dumb things

--- make_roc.py
+++ make_roc.py
@@ -9,7 +9,7 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('algo', type=str, help="'pull' or 'chi2' are the options if you used analyze.py to create your output files")
 parser.add_argument('subsystem', type=str, help='subsystem name matching the one that you gave analyze.py')
-# parser.add_argument('N', type=int, help='number of hists that fail threshold. Usually 1,3, or 5')
+parser.add_argument('N', type=int, help='number of hists that fail threshold. Usually 1,3, or 5')
 args = parser.parse_args()
 
 #pull_fig, pull_ax = plt.subplots()
@@ -18,13 +18,13 @@ args = parser.parse_args()
 #percNpull_fig, percNpull_ax = plt.subplots()
 #percNchi2_fig, percNchi2_ax = plt.subplots()
 #percNcomb_fig, percNcomb_ax = plt.subplots()
-#N = args.N
+N = args.N
 algo = args.algo
 subsystem = args.subsystem
 ## plot the pull and chi2 using rob's plotting scripts
 fig, axs = plt.subplots(ncols=2,nrows=1,figsize=(12,6))
 fig0, ax0 = plt.subplots(figsize=(6,6))
-
+fig1, ax1 = plt.subplots(figsize=(6,6))
 
 
 for numref, marker, color in zip(['1_REF', '4_REF', '8_REF'], ['-rD', '-bo', '-g^'], ['purple', 'yellow', 'orange']):
@@ -52,11 +52,6 @@ for numref, marker, color in zip(['1_REF', '4_REF', '8_REF'], ['-rD', '-bo', '-g
     zerothcut = sorted_df_g[0,:] + (sorted_df_g[0, :] - sorted_df_g[1,:])/2
     cuts = np.insert(cuts, 0, zerothcut, axis=0)
 
-    cutsIndex = None
-    #cuts_df = pd.DataFrame(cuts, columns=df.columns, index=)
-    print(cuts)
-    print(type(cuts))
-
     # print(sorted_df_g)
     #print(zerothcut)
 
@@ -80,77 +75,73 @@ for numref, marker, color in zip(['1_REF', '4_REF', '8_REF'], ['-rD', '-bo', '-g
     # pull_fig.savefig(plotdir+'avg_{}.png'.format(numref), bbox_inches='tight')
     # print('save ' + plotdir+'avg_{}.png'.format(numref))
 
+    # --------------- percent runs given N fails -----------------
+    perc_g = np.count_nonzero(counts_g > N, axis=1)/counts_g.shape[1]
+    perc_b = np.count_nonzero(counts_b > N, axis=1)/counts_b.shape[1]
+
+    # percNpull_ax.scatter(perc_g, perc_b , **plotkwargs)
+    # percNpull_ax.legend()
+    # percNpull_ax.set_title('fraction runs with N >= {} failing runs {}'.format(N, numref))
+    # percNpull_ax.set_xlabel('fraction good runs')
+    # percNpull_ax.set_ylabel('fraction bad runs')
+    # percNpull_ax.set_xlim(0,.5)
+    # percNpull_ax.set_ylim(0,1)
+    # percNpull_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
+    # percNpull_fig.savefig(plotdir+'percN_{}.png'.format(numref), bbox_inches='tight')
+    # print('save ' + plotdir+'percN_{}.png'.format(numref))
+    # print('--------------------------')
+
+
+
+    ##--------- plotting the output in the same way as rob --------------
+    ax1.set_xlabel('Fraction of good runs with at least N=3 histogram flags')
+    ax1.set_ylabel('Fraction of bad runs with at least N=3 histogram flags')
+    algorithm_name = "combined"
+
+    # commented out but keep for the aggregated scores plots
+    #for jj in range(len(N_bad_hists)):
+    #  print(N_bad_hists[jj])
+    #  print(tFRF_ROC_good_X[jj])
+    #  print(tFRF_ROC_bad_Y[jj])
+    ax1.plot(perc_g, perc_b, marker, mfc=color, mec='k', markersize=8, linewidth=1, label=numref)
+    ax1.axis(xmin=0,xmax=0.4,ymin=0,ymax=0.8)
+    ax1.axline((0, 0), slope=1, linestyle='--',linewidth=0.8,color='gray')
+    ax1.annotate(algorithm_name + " RF ROC", xy=(0.05, 0.95), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=12, weight='bold')
+    ax1.legend(loc='lower right')
+
+
     ax0.set_xlabel('Mean number of histogram flags per good run')
     ax0.set_ylabel('Mean number of histogram flags per bad run')
     ax0.plot(avg_cnt_g, avg_cnt_b, marker, mfc=color, mec='k', markersize=8, linewidth=1, label=numref)
     ax0.axline((0, 0), slope=1, linestyle='--',linewidth=0.8,color='gray')
-    ax0.annotate(algo + " HF ROC", xy=(0.05, 0.95), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=12, weight='bold')
+    ax0.annotate(algorithm_name + " HF ROC", xy=(0.05, 0.95), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=12, weight='bold')
     ax0.axis(xmin=0,xmax=8,ymin=0,ymax=25)
     ax0.legend(loc='lower right')
 
 
-    # --------------- percent runs given N fails -----------------
-    fig1, ax1 = plt.subplots(figsize=(6,6))
-    for N, m, c in zip([1,3,5], ['-rD', '-bo', '-g^'], ['purple', 'yellow', 'orange']):
-        perc_g = np.count_nonzero(counts_g > N, axis=1)/counts_g.shape[1]
-        perc_b = np.count_nonzero(counts_b > N, axis=1)/counts_b.shape[1]
+    ## --------------------------------------------------------------------
 
-        # percNpull_ax.scatter(perc_g, perc_b , **plotkwargs)
-        # percNpull_ax.legend()
-        # percNpull_ax.set_title('fraction runs with N >= {} failing runs {}'.format(N, numref))
-        # percNpull_ax.set_xlabel('fraction good runs')
-        # percNpull_ax.set_ylabel('fraction bad runs')
-        # percNpull_ax.set_xlim(0,.5)
-        # percNpull_ax.set_ylim(0,1)
-        # percNpull_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
-        # percNpull_fig.savefig(plotdir+'percN_{}.png'.format(numref), bbox_inches='tight')
-        # print('save ' + plotdir+'percN_{}.png'.format(numref))
-        # print('--------------------------')
+    # # at mean histogram good = 1.5, what is the threshold? So we need to look at the mean valeus
+    #def find_nearest(array, value):
+    #    array = np.asarray(array)
+    #    idx = (np.abs(array - value)).argmin()
+    #    return idx
 
+    ## score distribution for MH ==~ 1.5
+    #idx = find_nearest(avg_cnt_g, 1.5)
+    #
+    #cutsatMH = cuts[idx, :]
+    #fig, ax = plt.subplots()
+    #hist, bins, _ = ax.hist(cutsatMH, bins = 20)
+    #ax.cla()
+    #logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
+    #ax.hist(toplot, bins=logbins)
+    #ax.set_xscale('log')
+    #ax.set_title(f'distribution of {algo} threshold values at MH ~ 1.5')
+    #plt.savefig(f'plots/analysis/distributionmhh15_{algo}.png')
 
-
-        ##--------- plotting the output in the same way as rob --------------
-        ax1.set_xlabel(f'Fraction of good runs with at least N={N} histogram flags')
-        ax1.set_ylabel(f'Fraction of bad runs with at least N={N} histogram flags')
-        algorithm_name = algo
-
-        # commented out but keep for the aggregated scores plots
-        #for jj in range(len(N_bad_hists)):
-        #  print(N_bad_hists[jj])
-        #  print(tFRF_ROC_good_X[jj])
-        #  print(tFRF_ROC_bad_Y[jj])
-        ax1.plot(perc_g, perc_b, m, mfc=c, mec='k', markersize=8, linewidth=1, label=numref)
-        ax1.axis(xmin=0,xmax=0.4,ymin=0,ymax=0.8)
-        ax1.axline((0, 0), slope=1, linestyle='--',linewidth=0.8,color='gray')
-        ax1.annotate(algorithm_name + " RF ROC", xy=(0.05, 0.95), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=12, weight='bold')
-        ax1.legend(loc='lower right')
-
-
-
-
-        ## --------------------------------------------------------------------
-
-        # # at mean histogram good = 1.5, what is the threshold? So we need to look at the mean valeus
-        #def find_nearest(array, value):
-        #    array = np.asarray(array)
-        #    idx = (np.abs(array - value)).argmin()
-        #    return idx
-
-        ## score distribution for MH ==~ 1.5
-        #idx = find_nearest(avg_cnt_g, 1.5)
-        #
-        #cutsatMH = cuts[idx, :]
-        #fig, ax = plt.subplots()
-        #hist, bins, _ = ax.hist(cutsatMH, bins = 20)
-        #ax.cla()
-        #logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-        #ax.hist(toplot, bins=logbins)
-        #ax.set_xscale('log')
-        #ax.set_title(f'distribution of {algo} threshold values at MH ~ 1.5')
-        #plt.savefig(f'plots/analysis/distributionmhh15_{algo}.png')
-
-        fig1.savefig(plotdir + "RF_ROC_comparison_" + algorithm_name + "_" + numref + ".pdf",bbox_inches='tight')
-        print("SAVED: " + plotdir + "RF_ROC_comparison_" + algorithm_name + "_"+ numref  + ".pdf")
 
 fig0.savefig(plotdir + "HF_ROC_comparison_" + algorithm_name + ".pdf",bbox_inches='tight')
 print("SAVED: " + plotdir + "RF_HF_ROC_comparison_" + algorithm_name + ".pdf")
+fig1.savefig(plotdir + "RF_ROC_comparison_" + algorithm_name + ".pdf",bbox_inches='tight')
+print("SAVED: " + plotdir + "RF_ROC_comparison_" + algorithm_name + ".pdf")
