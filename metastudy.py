bcb3fcb1739a88ef54eea9e10a6d5870a16653ad
Chosila Sutantawibul <si_sutantawibul1@baylor.edu>
2024-07-31 19:49:01 +0200

    changed the legends size and labels

--- metastudy.py
+++ metastudy.py
@@ -1,127 +1,100 @@
+#### THIS IS THE VERSION WITH THE FORMAT FOR THE PAPER #####
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+import sys
+#np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+np.set_printoptions(threshold=sys.maxsize)
 ## !!!TODO::change this to take arguments of csv files instead of hard code xlsx file
-pull_fig, pull_ax = plt.subplots()
-chi2_fig, chi2_ax = plt.subplots()
-combined_fig, combined_ax = plt.subplots()
-percNpull_fig, percNpull_ax = plt.subplots()
-percNchi2_fig, percNchi2_ax = plt.subplots()
-percNcomb_fig, percNcomb_ax = plt.subplots()
-N = 1
+N = 3
+algo =   'chi2' # 'pull' #
+## plot the pull and chi2 using rob's plotting scripts
+fig, axs = plt.subplots(ncols=2,nrows=1,figsize=(12,6))
+fig0, ax0 = plt.subplots(figsize=(6,6))
+fig1, ax1 = plt.subplots(figsize=(6,6))
 
-for numref in ['1_REF', '4_REF', '8_REF']:
+for numref, marker, color in zip(['1_REF', '4_REF', '8_REF'], ['-D', '-o', '-^'], ["#e42536", "#f89c20", "#5790fc",]):#["#5790fc", "#f89c20","#7a21dd"]):#['purple', 'yellow', 'orange']):
     plotkwargs = {'label':numref,  'marker':'.'}
-    df = pd.read_excel('L1T_HLTPhysics.xlsx', sheet_name = '{}_L1THLTPhysics'.format(numref) )
+    # assumed all csv files are same name but in different directories
+    df = pd.read_csv('HLT_l1tShift_{}/L1T_HLTPhysics.csv'.format(numref))
+
     ## split into good (0,-1) and bad runs (1)
     df_g = df[df['label'] != 1]
     df_b = df[df['label'] == 1]
 
+
     ## remove bad rows (-99), col = ['label', 'run_number']
-    df_g = df_g.filter(regex='score')
+    df_g = df_g.filter(regex=f'{algo}_score') #'score')
     df_g = df_g[df_g != -99].dropna(how='all')
-    df_b = df_b.filter(regex='score')
+    df_b = df_b.filter(regex=f'{algo}_score')  # 'score')
     df_b = df_b[df_b != -99].dropna(how='all')
 
-    ## split into pull and chi2 and convert to numpy then sort descending
-    pull_g = -np.sort(-df_g.filter(regex='pull'), axis=0)
-    chi2_g = -np.sort(-df_g.filter(regex='chi2'), axis=0)
-    pull_b = -np.sort(-df_b.filter(regex='pull'), axis=0)
-    chi2_b = -np.sort(-df_b.filter(regex='chi2'), axis=0)
 
-    # -------------- number of counts failing thresholds ---------
+    print(df_b.columns)
+    import sys
+    sys.exit()
+
+    ## sort descending
+    sorted_df_g = -np.sort(-df_g, axis=0)
+
     ## calculate thresholds
-    pull_cuts = np.array([(col[1:] + col[:-1])/2 for col in pull_g.T]).T # first .T is so we can loop over columns instead of rows, the second .T converrts it back
-    zerothCut = pull_g[0,:] + (pull_g[0,:] - pull_g[1,:])/2
-    pull_cuts = np.insert(pull_cuts, 0, zerothCut, axis=0)
-
-    chi2_cuts = np.array([(col[1:] + col[:-1])/2 for col in chi2_g.T]).T
-    zerothCut = chi2_g[0,:] + (chi2_g[0,:] - chi2_g[1,:])/2
-    chi2_cuts = np.insert(chi2_cuts, 0, zerothCut, axis=0)
-
-    pull_cuts = pull_cuts
-    chi2_cuts = chi2_cuts
+    cuts = np.array([(col[1:] + col[:-1])/2 for col in sorted_df_g.T]).T
+    print(df_g.shape)
+    zerothcut = sorted_df_g[0,:] + (sorted_df_g[0, :] - sorted_df_g[1,:])/2
+    cuts = np.insert(cuts, 0, zerothcut, axis=0)
 
     ## get counts and mean
-    pull_counts_g = np.array([np.count_nonzero(df_g.filter(regex='pull') >= cut, axis=1) for cut in pull_cuts])
-    pull_counts_b = np.array([np.count_nonzero(df_b.filter(regex='pull')>= cut, axis=1) for cut in pull_cuts])
-    chi2_counts_g = np.array([np.count_nonzero(df_g.filter(regex='chi2') >= cut, axis=1) for cut in chi2_cuts])
-    chi2_counts_b = np.array([np.count_nonzero(df_b.filter(regex='chi2')>= cut, axis=1) for cut in chi2_cuts])
-
-    all_counts_g = pull_counts_g+chi2_counts_g#np.append(pull_counts_g, chi2_counts_g, axis=1)
-    all_counts_b = pull_counts_b+chi2_counts_b#np.append(pull_counts_b, chi2_counts_b, axis=1)
-
-    avg_pullcnt_g = pull_counts_g.mean(axis=1)
-    avg_pullcnt_b = pull_counts_b.mean(axis=1)
-    avg_chi2cnt_g = chi2_counts_g.mean(axis=1)
-    avg_chi2cnt_b = chi2_counts_b.mean(axis=1)
-    avg_allcnt_g  = all_counts_g.mean(axis=1)
-    avg_allcnt_b  = all_counts_b.mean(axis=1)
-
-
-    ## plot
-    pull_ax.scatter(avg_pullcnt_g, avg_pullcnt_b, **plotkwargs)
-    pull_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
-    pull_ax.set_xlim(0,5)
-    pull_ax.set_ylim(0,15)
-    pull_ax.set_title('average runs flagged: pull')
-    pull_ax.legend()
-    pull_ax.set_xlabel('average good runs flagged')
-    pull_ax.set_ylabel('average bad runs flagged')
-
-    chi2_ax.scatter(avg_chi2cnt_g, avg_chi2cnt_b , **plotkwargs)
-    chi2_ax.set_xlim(0,5)
-    chi2_ax.set_ylim(0,15)
-    chi2_ax.legend()
-    chi2_ax.set_title('average runs flagged: chi2')
-    chi2_ax.set_xlabel('average good runs flagged')
-    chi2_ax.set_ylabel('average bad runs flagged')
-    chi2_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
-
-    combined_ax.scatter(avg_allcnt_g, avg_allcnt_b, **plotkwargs)
-    combined_ax.legend()
-    combined_ax.set_xlim(0,5)
-    combined_ax.set_ylim(0,15)
-    combined_ax.set_title('average runs flagged: combined')
-    combined_ax.set_xlabel('average good runs flagged')
-    combined_ax.set_ylabel('average bad runs flagged')
-    combined_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
+    counts_g = np.array([np.count_nonzero(df_g >= cut, axis=1) for cut in cuts])
+    counts_b = np.array([np.count_nonzero(df_b >= cut, axis=1) for cut in cuts])
+    avg_cnt_g = counts_g.mean(axis=1)
+    avg_cnt_b = counts_b.mean(axis=1)
 
     # --------------- percent runs given N fails -----------------
-    perc_pull_g=np.count_nonzero(pull_counts_g >= N, axis=1)/pull_counts_g.shape[1]
-    perc_pull_b=np.count_nonzero(pull_counts_b >= N, axis=1)/pull_counts_b.shape[1]
-    perc_chi2_g=np.count_nonzero(chi2_counts_g >= N, axis=1)/chi2_counts_g.shape[1]
-    perc_chi2_b=np.count_nonzero(chi2_counts_b >= N, axis=1)/chi2_counts_b.shape[1]
-    perc_all_g =np.count_nonzero(all_counts_g >= N, axis=1)/all_counts_g.shape[1]
-    perc_all_b =np.count_nonzero(all_counts_b >= N, axis=1)/all_counts_b.shape[1]
+    perc_g = np.count_nonzero(counts_g > N, axis=1)/counts_g.shape[1]
+    perc_b = np.count_nonzero(counts_b > N, axis=1)/counts_b.shape[1]
 
-    percNpull_ax.scatter(perc_pull_g, perc_pull_b , **plotkwargs)
-    percNpull_ax.legend()
-    percNpull_ax.set_title('fraction runs with N >= {} failing runs pull cut'.format(N))
-    percNpull_ax.set_xlabel('fraction good runs')
-    percNpull_ax.set_ylabel('fraction bad runs')
-    percNpull_ax.set_xlim(0,.5)
-    percNpull_ax.set_ylim(0,1)
-    percNpull_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
+    ##--------- plotting the output in the same way as rob --------------
+    # algorithm_name = "combined"
+    algorithm_name = "chi2" if algo=='chi2' else "max pull"
+    xaxislabelsize = 14
+    yaxislabelsize = xaxislabelsize
+    cmssize = 18
+    luminositysize = 14
+    axisnumbersize = 12
+    annotatesize=16
+    labelpad=10
+    linewidth=1.5
+    legendlabel = f'{numref[0]} reference runs' if numref[0] != '1' else f'{numref[0]} reference run'
+    ax1.tick_params(axis='both', which='major', labelsize=axisnumbersize)
+    ax1.set_xlabel(f'Fraction of good runs with ≥{N} histogram flags', fontsize=xaxislabelsize, labelpad=labelpad)
+    ax1.set_ylabel(f'Fraction of bad runs with ≥{N} histogram flags', fontsize=yaxislabelsize, labelpad=labelpad)
+    ax1.axline((0, 0), slope=1, linestyle='--', linewidth=linewidth, color='#964a8b', zorder=0)
+    ax1.plot(perc_g, perc_b, marker, mfc=color, color=color, mec='k', markersize=8, linewidth=1, label=legendlabel)
+    ax1.axis(xmin=0,xmax=0.4,ymin=0,ymax=0.8)
+    ax1.annotate(f"Beta-binomial {algorithm_name} test", xy=(0.05, 0.98), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=16, fontstyle='italic')
+    ax1.legend(loc='lower right', fontsize=annotatesize)
+    ax1.text(0, 1.02, "CMS", fontsize=cmssize, weight='bold',transform=ax1.transAxes)
+    ax1.text(0.15, 1.02, "Preliminary", fontsize=cmssize-4, fontstyle='italic', transform=ax1.transAxes)
+    #ax1.text(0, 1.02, "Private work (CMS data)", fontsize=14, fontstyle='italic', transform=ax1.transAxes)
+    ax1.text(0.63, 1.02, "2022 (13.6 TeV)", fontsize=14, transform=ax1.transAxes)
 
-    percNchi2_ax.scatter(perc_chi2_g, perc_chi2_b , **plotkwargs)
-    percNchi2_ax.legend()
-    percNchi2_ax.set_title('fraction runs with N >= {} failing runs chi2 cut'.format(N))
-    percNchi2_ax.set_xlabel('fraction good runs')
-    percNchi2_ax.set_ylabel('fraction bad runs')
-    percNchi2_ax.set_xlim(0,.5)
-    percNchi2_ax.set_ylim(0,1)
-    percNchi2_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
+    ax0.tick_params(axis='both', which='major', labelsize=axisnumbersize)
+    ax0.set_xlabel('Mean histogram flags per good run', fontsize=xaxislabelsize, labelpad=labelpad)
+    ax0.set_ylabel('Mean histogram flags per bad run', fontsize=yaxislabelsize, labelpad=labelpad)
+    ax0.axline((0, 0), slope=1, linestyle='--',linewidth=linewidth,color='#964a8b', zorder=0)
+    ax0.plot(avg_cnt_g, avg_cnt_b, marker, mfc=color, color=color, mec='k', markersize=8, linewidth=1, label=legendlabel)
+    ax0.annotate(f"Beta-binomial {algorithm_name} test", xy=(0.05, 0.98), xycoords='axes fraction', xytext=(10, -10), textcoords='offset points', ha='left', va='top', fontsize=annotatesize, fontstyle='italic')
+    ax0.axis(xmin=0,xmax=8,ymin=0,ymax=25)
+    ax0.legend(loc='center left', fontsize=annotatesize, bbox_to_anchor=(0.05, 0.75), bbox_transform=ax0.transAxes)
+    #ax0.text(0, 1.02, "CMS", fontsize=cmssize, weight='bold', transform=ax0.transAxes)
+    #ax0.text(0.15, 1.02, "Preliminary", fontsize=cmssize-4, fontstyle='italic', transform=ax0.transAxes)
+    ax0.text(0, 1.02, "Private work (CMS data)", fontsize=cmssize-4, fontstyle='italic', transform=ax0.transAxes)
+    ax0.text(0.63, 1.02, "2022 (13.6 TeV)", fontsize=luminositysize, transform=ax0.transAxes)
+    ## --------------------------------------------------------------------
 
-    percNcomb_ax.scatter(perc_all_g, perc_all_b, **plotkwargs)
-    percNcomb_ax.set_title('fraction runs with N >= {} failing runs chi2 or pull cut'.format(N))
-    percNcomb_ax.set_xlabel('fraction good runs')
-    percNcomb_ax.set_ylabel('fraction bad runs')
-    percNcomb_ax.set_xlim(0,.5)
-    percNcomb_ax.set_ylim(0,1)
-    box = percNcomb_ax.get_position()
-    percNcomb_ax.legend()
-    percNcomb_ax.plot(range(0,10), range(0,10), color='r', linewidth=1, linestyle='--')
-    # percNcomb_ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-    # percNcomb_ax.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+#for cut in cuts[:4]:
+#    print(cut)
+fig0.savefig("HF_ROC_comparison_" + algorithm_name + ".png",bbox_inches='tight')
+#print("SAVED: " + args.output_dir + "/RF_HF_ROC_comparison_" + algorithm_name + ".pdf")
+fig1.savefig("RF_ROC_comparison_" + algorithm_name + ".png",bbox_inches='tight')
